1609af84f642b28ab0a021380d7046cc500bc4bd
aaaassssqqqq <42968162+aaaassssqqqq@users.noreply.github.com>
2018-10-16 06:16:03 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-client.on('ready', () => {
-  client.user.setGame(`....');
-var prefix = "$";
+
 
 
 client.login(process.env.BOT_TOKEN);
