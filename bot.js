e3e74e1173a10595984bd3a00b205ee59061372f
aaaassssqqqq <42968162+aaaassssqqqq@users.noreply.github.com>
2018-10-23 14:21:33 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,25 +3,6 @@ const client = new Discord.Client();
 var prefix = "$";
 client.on('message', message => {
 
-              if(!message.channel.guild) return;
-
-client.on('message', message => {
-   
-    let args = message.content.split(' ').slice(1).join(' ');
-   
-  if (message.content === 'ping') {
-      message.channel.send(`<@${message.author.id}> Ping..!`)
-  }
- 
- 
-  if (message.content.startsWith('$bc')) {
-          if (!args[0]) {
-message.channel.send("**$bc <message>**");
-return;
-}
-message.guild.members.forEach(m => {
-   if(!message.member.hasPermission('ADMINISTRATOR')) return;
-   m.send(`${args}`);
  
 });
  
