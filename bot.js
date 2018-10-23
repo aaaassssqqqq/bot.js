9c9dbac8182d69df280ad27ec390fa8cf1085615
aaaassssqqqq <42968162+aaaassssqqqq@users.noreply.github.com>
2018-10-23 14:56:23 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,8 @@ if (message.author.id === client.user.id) return;
 if (message.guild) {
 let embed = new Discord.RichEmbed()
 let args = message.content.split(' ').slice(1).join(' ');
-if(message.content.split(' ')[0] == # + 'bc') {
+  const prefix = "#";
+if(message.content.split(' ')[0] == prefix + 'bc') {
 if(!message.channel.guild) return message.reply('**❌ اسف لكن هذا الامر للسيرفرات فقط **');         
 if (!args[1]) {
 return;
