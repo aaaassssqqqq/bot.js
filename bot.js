fb10878ac6df5b734e1c1908e9743b4f237c6388
aaaassssqqqq <42968162+aaaassssqqqq@users.noreply.github.com>
2018-10-23 15:10:18 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,8 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const prefix = "$";
-cclient.on("message", message => {
-    if (message.content.startsWith("$bc")) {
+client.on("message", message => {
+    if (message.content.startsWith("#obc")) {
                  if (!message.member.hasPermission("ADMINISTRATOR"))  return;
   let args = message.content.split(" ").slice(1);
   var argresult = args.join(' ');
