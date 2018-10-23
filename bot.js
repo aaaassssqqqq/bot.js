fa2dd718104d16861a12c9161f2199981b650f20
aaaassssqqqq <42968162+aaaassssqqqq@users.noreply.github.com>
2018-10-23 14:41:04 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,6 +26,39 @@ client.on('ready', () => {
 client.user.setGame(`Winter`,"http://twitch.tv/S-F")
 client.user.setStatus("dnd")
 });
+client.on('message', message => {
+    if (message.content.startsWith("رابط")) {
+
+  message.channel.createInvite({
+        thing: true,
+        maxUses: 2,
+        maxAge: 86400
+    }).then(invite =>
+      message.author.sendMessage(invite.url)
+    )
+  message.channel.send("**تم ارسال الرابط برسالة خاصة**")
+
+message.author.send(`**مدة الرابط : يـوم
+عدد استخدامات الرابط : 2**`)
 
 
+    }
+});
+client.on('guildMemberAdd', Sal => { // By:Dexter
+    var embed = new Discord.RichEmbed()
+    .setAuthor(Sal.user.username, Sal.user.avatarURL)
+    .setThumbnail(Sal.user.avatarURL)
+    .setImage('http://live-timely-4jepdssgmc.time.ly/wp-content/uploads/2018/08/welcomeEvents.jpg') //هنا حط الصوره الي تبيها
+    .setTitle('عضو جديد!')
+    .setDescription('مرحبا بك بالسيرفر')
+    .addField('``ايدي العضو``:',"" +  Sal.user.id, true)
+    .addField('``تاق العضو``', Sal.user.discriminator, true)
+    .addField('``تم الانشاء في``', Sal.user.createdAt, true)
+    .addField(' 👤  انت رقم',`**[ ${Sal.guild.memberCount} ]**`,true)
+    .setColor('RANDOM')
+    .setFooter(Sal.guild.name, Sal.guild.iconURL, true)
+    var channel =Sal.guild.channels.find('name', 'welcome') //ww
+    if (!channel) return;
+    channel.send({embed : embed});
+    });
 client.login(process.env.BOT_TOKEN);
