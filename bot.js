b84f01bf15b157c7c81d71ee0f4a582bb1a31333
aaaassssqqqq <42968162+aaaassssqqqq@users.noreply.github.com>
2018-10-23 15:32:51 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,18 +1,28 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const prefix = "$";
-client.on("message", message => {
-    if (message.content.startsWith("#obc")) {
-                 if (!message.member.hasPermission("ADMINISTRATOR"))  return;
-  let args = message.content.split(" ").slice(1);
-  var argresult = args.join(' ');
-  message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
-  m.send(${argresult}\n ${m});
-  })
-  message.channel.send(\${message.guild.members.filter( m => m.presence.status !== 'all').size}`mailbox  عدد المستلمين `);
-  message.delete();
-  };
-  });
+client.on('message', message => {
+   
+    let args = message.content.split(' ').slice(1).join(' ');
+   
+  if (message.content === 'ping') {
+      message.channel.send(`<@${message.author.id}> Ping..!`)
+  }
+ 
+ 
+  if (message.content.startsWith('#bc')) {
+          if (!args[0]) {
+message.channel.send("**#bc <message>**");
+return;
+}
+message.guild.members.forEach(m => {
+   if(!message.member.hasPermission('ADMINISTRATOR')) return;
+   m.send(`${args}`);
+ 
+});
+  }
+ 
+});
 client.on('ready', () => {
    console.log(`----------------`);
       console.log(`Desert Bot- Script By : i1Suhaib`);
